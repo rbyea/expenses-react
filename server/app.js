b799bd205e519787f9403e72663e0a3e44ca2b70
Egor <csgo.roby@gmail.com>
2023-11-04 15:22:01 +0300

    add client and server MongoDB

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+const initDatabase = require("./startUp/initDatabase");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = config.get("port") ?? 8080;
+
+// if (process.env.NODE_ENV === "production") {
+//   console.log("prod");
+// }
+
+//  flrbyave
+// 6LFBaCZJeBbAmzyy
+
+async function start() {
+  try {
+    mongoose.connection.once("open", () => {
+      initDatabase()
+    })
+    await mongoose.connect(config.get('mongoUri'));
+    console.log(chalk.green(`MongoDB connected...`))
+    app.listen(8080, () =>
+      console.log(chalk.green(`Server has been started om port ${PORT}...`))
+    );
+  } catch (error) {
+    console.log(chalk.red(error.message));
+    process.exit(1);
+  }
+}
+
+start();
